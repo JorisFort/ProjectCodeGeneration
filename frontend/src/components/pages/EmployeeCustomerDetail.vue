ba9c47e90b697ed24b1db767d05174e81653e310
KimvanSchagen <95349001+KimvanSchagen@users.noreply.github.com>
2024-06-07 15:39:03 +0200

    Employee delete customer account, frontend

--- frontend/src/components/pages/EmployeeCustomerDetail.vue
+++ frontend/src/components/pages/EmployeeCustomerDetail.vue
@@ -3,8 +3,22 @@ import EmployeeNavigation from "../common/EmployeeNavigation.vue";
 import EmployeeCustomerDetailOverview from "../containers/EmployeeCustomerDetailOverview.vue";
 
 import {useRoute} from "vue-router";
+import {computed, ref} from "vue";
 const id = useRoute().params.id;
 
+const confirmDeletion = ref(false);
+
+const confirmingDeletion = () => {
+  confirmDeletion.value = true;
+};
+
+const noDeletion = () => {
+  confirmDeletion.value = false;
+};
+
+const deleteAccount = () => {
+
+};
 </script>
 
 <template>
@@ -13,15 +27,23 @@ const id = useRoute().params.id;
     <EmployeeNavigation />
   </div>
   <div class="employeeContent">
-    <div>
-      <router-link to="/EmployeeCustomers" class="backButton"><button>Back</button></router-link>
-    </div>
-    <div>
-      <EmployeeCustomerDetailOverview :id = "id"/>
-    </div>
-    <div class="grid">
+    <div v-if="!confirmDeletion">
+      <div>
+        <router-link to="/EmployeeCustomers" class="backButton"><button>Back</button></router-link>
+      </div>
+      <div>
+        <EmployeeCustomerDetailOverview :id = "id"/>
+      </div>
+      <div class="grid">
+        <button @click="confirmingDeletion" class="employeeButton">Close account</button>
+      </div>
     </div>
 
+    <div v-if="confirmDeletion">
+      <h3>Are you sure you want to close this account?</h3>
+        <button @click="deleteAccount" class="employeeDeletionButton">Yes</button>
+        <button @click="noDeletion" class="employeeDeletionButton">No</button>
+    </div>
   </div>
 
 </template>
