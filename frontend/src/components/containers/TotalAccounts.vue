634f291d4cc957c4537ed4108fbf0b2b0c3d4df8
KimvanSchagen <95349001+KimvanSchagen@users.noreply.github.com>
2024-06-07 14:15:09 +0200

    dashboard

---
+++ frontend/src/components/containers/TotalAccounts.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="dashboardSquare">
+    <p>Total accounts:</p>
+    <p> {{ accounts }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    const accounts = 250000;
+
+    return {
+      accounts
+    }
+  }
+}
+</script>
