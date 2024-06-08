d7e5a671a02fdcfdb2d7b1718ea124e3db0a9866
KimvanSchagen <95349001+KimvanSchagen@users.noreply.github.com>
2024-06-08 13:58:48 +0200

    Employee own account details and changing account, frontend

--- frontend/src/components/pages/EmployeeEditAccount.vue
+++ frontend/src/components/pages/EmployeeEditAccount.vue
@@ -1,5 +1,6 @@
 <script setup>
 import EmployeeNavigation from "../common/EmployeeNavigation.vue";
+import EmployeeChangeAccountForm from "../containers/EmployeeChangeAccountForm.vue";
 </script>
 
 <template>
@@ -7,5 +8,9 @@ import EmployeeNavigation from "../common/EmployeeNavigation.vue";
     <EmployeeNavigation />
   </div>
   <div class="employeeContent">
+    <div>
+      <router-link to="/EmployeeAccount" class="backButton"><button>Back</button></router-link>
+    </div>
+    <EmployeeChangeAccountForm/>
   </div>
 </template>
